c95ef1472a41b7eb4377fb1fb4834075daf251d3
Adam Del Castillo-Call <adam.delcastillocall6882@stu.mtec.edu>
2022-06-08 16:15:36 -0600

    first commit. I have the entire assignment done.

---
+++ Week5Day4Project/app.js
@@ -0,0 +1,10 @@
+$('.navItem').each(function() {
+    $(this).css('color', 'white');
+})
+
+$('#nav').css('background-color', '#145');
+
+$('ul :even').css('color', '#ccc');
+$('ul :odd').css('color', '#aaa');
+
+$('input').val('Adam');
